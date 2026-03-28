b7bae3e5cb91dd1402a1ee38103fd0b9ac7e166a
Vinay Divakar <vinaydivakar1990@gmail.com>
2026-03-28 19:37:55 +1300

    feat(adc): configure SAADC channel for KS0171 pulse sensor
    Replace example application boilerplate with nRF52840 SAADC setup.
    Configures AIN1 with async ADC, RTT logging and 100-sample sequence at 100Hz.

--- app/src/main.c
+++ app/src/main.c
@@ -3,78 +3,43 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <app_version.h>
+#include <zephyr/drivers/adc.h>
 #include <zephyr/kernel.h>
-#include <zephyr/drivers/sensor.h>
 #include <zephyr/logging/log.h>
 
-#include <app/drivers/blink.h>
+LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
-#include <app_version.h>
+static const struct adc_dt_spec adc_chan =
+    ADC_DT_SPEC_GET(DT_PATH(zephyr_user));
 
-LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
+static int16_t _buffer[100] = {};
 
-#define BLINK_PERIOD_MS_STEP 100U
-#define BLINK_PERIOD_MS_MAX  1000U
+static struct adc_sequence_options _options = {
+    .interval_us = 10000 /* 10 ms */,
+    .extra_samplings = 99 /* 99 extra + 1 = 100 total */,
+    .callback = NULL,
+    .user_data = NULL,
+};
 
-int main(void)
-{
-	int ret;
-	unsigned int period_ms = BLINK_PERIOD_MS_MAX;
-	const struct device *sensor, *blink;
-	struct sensor_value last_val = { 0 }, val;
+static struct adc_sequence _config = {
+    .buffer = _buffer,
+    .buffer_size = sizeof(_buffer),
+    .options = &_options,
+    .calibrate = false,
+};
 
-	printk("Zephyr Example Application %s\n", APP_VERSION_STRING);
+int main(void) {
+  LOG_INF("Heart Rate Monitor App version: %s", APP_VERSION_STRING);
 
-	sensor = DEVICE_DT_GET(DT_NODELABEL(example_sensor));
-	if (!device_is_ready(sensor)) {
-		LOG_ERR("Sensor not ready");
-		return 0;
-	}
+  int err = adc_is_ready_dt(&adc_chan);
+  if (err) {
+    LOG_ERR("ADC device not ready");
+    return err;
+  }
 
-	blink = DEVICE_DT_GET(DT_NODELABEL(blink_led));
-	if (!device_is_ready(blink)) {
-		LOG_ERR("Blink LED not ready");
-		return 0;
-	}
+  while (1) {
+  }
 
-	ret = blink_off(blink);
-	if (ret < 0) {
-		LOG_ERR("Could not turn off LED (%d)", ret);
-		return 0;
-	}
-
-	printk("Use the sensor to change LED blinking period\n");
-
-	while (1) {
-		ret = sensor_sample_fetch(sensor);
-		if (ret < 0) {
-			LOG_ERR("Could not fetch sample (%d)", ret);
-			return 0;
-		}
-
-		ret = sensor_channel_get(sensor, SENSOR_CHAN_PROX, &val);
-		if (ret < 0) {
-			LOG_ERR("Could not get sample (%d)", ret);
-			return 0;
-		}
-
-		if ((last_val.val1 == 0) && (val.val1 == 1)) {
-			if (period_ms == 0U) {
-				period_ms = BLINK_PERIOD_MS_MAX;
-			} else {
-				period_ms -= BLINK_PERIOD_MS_STEP;
-			}
-
-			printk("Proximity detected, setting LED period to %u ms\n",
-			       period_ms);
-			blink_set_period_ms(blink, period_ms);
-		}
-
-		last_val = val;
-
-		k_sleep(K_MSEC(100));
-	}
-
-	return 0;
+  return 0;
 }
-
